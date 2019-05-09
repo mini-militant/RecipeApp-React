67912f4cee2fc40c87fa6069dbc50650efd1502e
mini-militant <shailesh.coolcalm007@gmail.com>
2019-05-09 20:58:39 +0530

    1 file modified

--- app.js
+++ app.js
@@ -6,10 +6,19 @@ const API_KEY="91a91e6a73bffff78256088bda385737" ;
 
 class App extends Component{
 
-   getRecipe=(e)=>{
+    state={
+      recipes:[]
+    }
+
+   getRecipe=async(e)=>{
     e.preventDefault();
     const recipeName = e.target.elements.recipeName.value;
-    console.log(recipeName);
+    const api_url=await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=15`);
+    const data = await api_url.json();
+    console.log(data);
+    this.setState({
+      recipes:data.recipes
+    })
   }
 
    render() {
@@ -18,7 +27,8 @@ class App extends Component{
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
-        <Form getRecipe={this.getRecipe}/>
+        <Form 
+        getRecipe={this.getRecipe}/>
       </div>
     );
   }
