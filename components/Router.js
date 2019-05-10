4ca33fbbd91658456f843b676ab6be0c386efb58
mini-militant <shailesh.coolcalm007@gmail.com>
2019-05-10 09:55:40 +0530

    2 files modified

--- components/Router.js
+++ components/Router.js
@@ -1,3 +1,14 @@
-import React from "react";
+import React from 'react';
+import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import App from '../app'
+
+const Router=()=>{
+  return(
+  <BrowserRouter>
+    <Route path="/" component={App}/>
+  </BrowserRouter>
+  );
+  
+}
 
 export default Router;
